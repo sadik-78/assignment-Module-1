860d13e1465cbad572c8e68cd72b248b08683b81
sadik <maitursadik0121@gmail.com>
2026-09-04 00:39:30 +0600

    assignment-Module-1

---
+++ problem1.py
@@ -0,0 +1,21 @@
+a = input("enter your name:")
+math= int(input("enter your MATH subject number:"))
+ict = int(input("enter your ICT subject number:"))
+bangla = int(input("enter your BANGLA subject number:"))
+
+print(f"Student name:{a}")
+total_mark=math+ict+bangla
+print(f"Total mark:{total_mark}")
+avg=total_mark/3
+print(f"Average:{avg:.2f}")
+
+if 80<= avg <=100:
+    print("Grade:A+")
+elif  70<= avg <=79:
+    print("Grade:A")
+elif 60<= avg <=69:
+    print("Grade:B")
+elif 50<= avg <=59:
+    print("Grade:C")
+else:
+    print("Grade:F") 
