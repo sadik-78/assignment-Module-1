221b855f18b5ddb8ce591478d719f90323b85a22
sadik <maitursadik0121@gmail.com>
2026-09-04 16:14:12 +0600

    edite some empty space

--- problem2.py
+++ problem2.py
@@ -9,35 +9,39 @@ y=int(input("price:"))
 p=input("enter the 3rd product name:")
 z=int(input("price:"))
 
-
+print()
 print(f"Customer Name:{a}")
-
+print()
 print(f"product 1:{b}")
 print(f"price:{c}")
-
-print(f"product 1:{x}")
+print()
+print(f"product 2:{x}")
 print(f"price:{y}")
-
-print(f"product 1:{p}")
+print()
+print(f"product 3:{p}")
 print(f"price:{z}")
-
+print()
 total=c+y+z
 print(f"Subtotal:{total}")
+print()
 
 if total>=5000:
     f=total*0.2
     g=total-f
     print(f"Discount:{f:.2f}")
+    print()
     print(f"Final Total:{g:.2f}")
 elif 3000<= total <=4999:
     h=total*0.1
     j=total-h
     print(f"Discount:{h:.2f}")
+    print()
     print(f"Final Total:{j:.2f}")
 elif 1000<= total <=2999:
     k=total*0.05
     l=total-k
     print(f"Discount:{k:.2f}")
+    print()
     print(f"Final Total:{l:.2f}")
 else:
     print(f"NO DISCOUNT")
