6a55fa60b60cf9acf8d2db4bacca8617fd308829
sadik <maitursadik0121@gmail.com>
2026-09-04 16:21:17 +0600

    adding some empty space

--- problem1.py
+++ problem1.py
@@ -2,12 +2,16 @@ a = input("enter your name:")
 math= int(input("enter your MATH subject number:"))
 ict = int(input("enter your ICT subject number:"))
 bangla = int(input("enter your BANGLA subject number:"))
-
+print()
 print(f"Student name:{a}")
+print()
 total_mark=math+ict+bangla
 print(f"Total mark:{total_mark}")
+print()
+
 avg=total_mark/3
 print(f"Average:{avg:.2f}")
+print()
 
 if 80<= avg <=100:
     print("Grade:A+")
